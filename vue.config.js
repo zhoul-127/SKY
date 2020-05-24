5842108197855d8229d423f44aadea5eff3d008e
474565076@qq.com <ZHOULU124205zl>
2020-05-24 16:28:33 +0800

    引用修改

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,20 @@
 // vue.config.js
 module.exports = {
-    devServer:{
-      proxy:{
-		  '/api':{
-            target:`http://39.99.173.172:8082/api`,
-            changeOrigin:true,
-            ws: true,
-            pathRewrite:{
-              '^/api':'',
-            }
-		  }
-        }
-      }
-    }
+	lintOnSave: false,
+	devServer: {
+		overlay: {
+			warnings: false,
+			errors: false
+		},
+		proxy: {
+			'/api': {
+				target: `http://39.99.173.172:8082/api`,
+				changeOrigin: true,
+				ws: true,
+				pathRewrite: {
+					'^/api': '',
+				}
+			}
+		}
+	}
+}
